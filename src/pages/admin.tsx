fd3b610259ceec5c82faae4c44193575d9b34ff2
Kevin Peredo <kveperedo@gmail.com>
2023-04-16 15:53:10 +0800

    [feat] Add deleting of clinics

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -6,12 +6,49 @@ import { api } from '~/utils/api';
 import { useState } from 'react';
 import type { ChangeEvent } from 'react';
 import Pagination from '~/components/Pagination';
-import { ListPlus, MagnifyingGlass } from '@phosphor-icons/react';
+import { ListPlus, MagnifyingGlass, Trash } from '@phosphor-icons/react';
 import ScrollArea from '~/components/ScrollArea';
 import { twMerge } from 'tailwind-merge';
 import Input from '~/components/Input';
 import Loading from '~/components/Loading';
 import Head from 'next/head';
+import AlertDialog from '~/components/AlertDialog';
+import { useToast } from '~/hooks/useToast';
+import Tooltip from '~/components/Tooltip';
+
+const RemoveClinicDialog = ({ onConfirm }: { onConfirm: () => void }) => {
+    return (
+        <AlertDialog.Root>
+            <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                    <AlertDialog.Trigger asChild>
+                        <button className='default-focus m-auto flex h-8 w-8 items-center justify-center rounded-full transition-colors hover:bg-zinc-200 active:bg-zinc-300'>
+                            <Trash weight='fill' className='text-zinc-600' />
+                        </button>
+                    </AlertDialog.Trigger>
+                </Tooltip.Trigger>
+                <Tooltip.Content>Delete Clinic</Tooltip.Content>
+            </Tooltip.Root>
+
+            <AlertDialog.Content>
+                <AlertDialog.Header>
+                    <AlertDialog.Title>Remove Clinic?</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        Are you sure you want to remove this clinic? All users
+                        linked will not have access to the clinic&apos;s records
+                        anymore.
+                    </AlertDialog.Description>
+                </AlertDialog.Header>
+                <AlertDialog.Footer>
+                    <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+                    <AlertDialog.Action onClick={onConfirm}>
+                        Remove clinic
+                    </AlertDialog.Action>
+                </AlertDialog.Footer>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
+    );
+};
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const session = await getServerAuthSession(ctx);
@@ -31,12 +68,25 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const AdminPage: NextPageWithLayout = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [pageNumber, setPageNumber] = useState(1);
-    const { data: pageCount } = api.admin.getClinicPageCount.useQuery(searchTerm);
+    const utils = api.useContext();
+    const { data: pageCount } =
+        api.admin.getClinicPageCount.useQuery(searchTerm);
     const { data, isLoading } = api.admin.listClinics.useQuery({
         pageNumber,
         searchTerm,
     });
-    const areRecordsLoading = isLoading || !data;
+    const { mutate: deleteClinic, isLoading: isDeletingClinic } =
+        api.admin.deleteClinic.useMutation({
+            onSuccess: () => {
+                toast({
+                    title: 'Delete Clinic',
+                    description: `Successfully deleted clinic.`,
+                });
+                void utils.admin.listClinics.invalidate();
+            },
+        });
+    const { toast } = useToast();
+    const areRecordsLoading = isLoading || !data || isDeletingClinic;
 
     const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -62,7 +112,7 @@ const AdminPage: NextPageWithLayout = () => {
                 <title>Admin Panel</title>
             </Head>
             <div className='flex h-full flex-1'>
-                <div className='m-2 flex flex-col rounded border border-zinc-200 bg-zinc-50 sm:mx-auto sm:my-8 sm:w-[768px]'>
+                <div className='m-2 flex w-full flex-col rounded border border-zinc-200 bg-zinc-50 sm:mx-auto sm:my-8 sm:w-[768px]'>
                     <header className='mb-6 flex items-center justify-between gap-4 p-4'>
                         <button className='flex items-center justify-center gap-2 rounded bg-zinc-700 p-2 text-zinc-200 transition-colors hover:bg-zinc-800 sm:px-6 sm:py-2'>
                             <ListPlus className='h-6 w-6 text-inherit' />
@@ -76,7 +126,7 @@ const AdminPage: NextPageWithLayout = () => {
                                 endIcon={
                                     <button
                                         type='submit'
-                                        className='flex h-full w-full items-center justify-center'
+                                        className='default-focus flex h-full w-full items-center justify-center rounded'
                                     >
                                         <MagnifyingGlass className='h-6 w-6 text-inherit' />
                                     </button>
@@ -88,12 +138,13 @@ const AdminPage: NextPageWithLayout = () => {
 
                     <main className='flex h-32 flex-1 flex-col overflow-hidden'>
                         <div className='flex border-b border-zinc-200 shadow-sm'>
-                            <div className='basis-1/2 px-4 py-2 text-left text-sm font-semibold text-zinc-700'>
+                            <div className='basis-3/6 px-4 py-2 text-left text-sm font-semibold text-zinc-700'>
                                 Clinic Name
                             </div>
-                            <div className='basis-1/2 px-4 py-2 text-left text-sm font-semibold text-zinc-700'>
+                            <div className='basis-2/6 px-4 py-2 text-left text-sm font-semibold text-zinc-700'>
                                 Address
                             </div>
+                            <div className='basis-1/6 px-4 py-2 text-center text-sm font-semibold text-zinc-700'></div>
                         </div>
                         {areRecordsLoading ? (
                             <div className='flex-1'>
@@ -108,12 +159,12 @@ const AdminPage: NextPageWithLayout = () => {
 
                                         return (
                                             <div
-                                                className='flex cursor-pointer transition-colors hover:bg-zinc-100'
+                                                className='flex cursor-pointer transition-colors hover:bg-zinc-100 active:bg-zinc-200'
                                                 key={clinic.id}
                                             >
                                                 <div
                                                     className={twMerge(
-                                                        'basis-1/2 border-b border-zinc-200 px-4 py-4',
+                                                        'basis-3/6 border-b border-zinc-200 px-4 py-4',
                                                         isLast &&
                                                             'border-transparent'
                                                     )}
@@ -122,12 +173,27 @@ const AdminPage: NextPageWithLayout = () => {
                                                 </div>
                                                 <div
                                                     className={twMerge(
-                                                        'basis-1/2 border-b border-zinc-200 px-4 py-4',
+                                                        'basis-2/6 border-b border-zinc-200 px-4 py-4',
                                                         isLast &&
                                                             'border-transparent'
                                                     )}
                                                 >
                                                     {clinic.address}
+                                                </div>
+                                                <div
+                                                    className={twMerge(
+                                                        'flex basis-1/6 border-b border-zinc-200 px-4 py-2',
+                                                        isLast &&
+                                                            'border-transparent'
+                                                    )}
+                                                >
+                                                    <RemoveClinicDialog
+                                                        onConfirm={() =>
+                                                            deleteClinic(
+                                                                clinic.id
+                                                            )
+                                                        }
+                                                    />
                                                 </div>
                                             </div>
                                         );
