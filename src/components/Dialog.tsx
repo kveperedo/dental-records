c85fc98085e4060bda28bce57bbc0c147204797f
Kevin Peredo <kveperedo@gmail.com>
2023-04-22 12:05:03 +0800

    [feat] Update Dialog layout on mobile

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -70,7 +70,7 @@ const Content = forwardRef<
         <DialogPrimitive.Content
             ref={ref}
             className={twMerge(
-                'fixed z-50 grid w-full gap-4 rounded-b-lg border bg-zinc-50 p-6 shadow-lg animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 sm:max-w-lg sm:rounded-lg sm:zoom-in-90 data-[state=open]:sm:slide-in-from-bottom-0',
+                'fixed bottom-0 z-50 grid w-full gap-4 rounded-b-lg border bg-zinc-50 p-6 shadow-lg animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 sm:bottom-auto sm:max-w-lg sm:rounded-lg sm:zoom-in-90 data-[state=open]:sm:slide-in-from-bottom-0',
                 className
             )}
             {...props}
