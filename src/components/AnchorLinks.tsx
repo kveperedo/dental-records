d7627a446c7c03ecec504060ddc621e53e8e9793
Kevin Peredo <kveperedo@gmail.com>
2023-04-24 08:58:16 +0800

    [feat] Fix AnchorLinks implementation

--- src/components/AnchorLinks.tsx
+++ src/components/AnchorLinks.tsx
@@ -3,6 +3,7 @@ import type { Icon } from '@phosphor-icons/react';
 import { useRouter } from 'next/router';
 import type { PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
+import NextLink from 'next/link';
 
 type LinkProps = PropsWithChildren<{
     href: string;
@@ -11,21 +12,19 @@ type LinkProps = PropsWithChildren<{
 }>;
 
 const Link = ({ href, Icon, active, children }: LinkProps) => {
-    const router = useRouter();
-
     return (
-        <a
+        <NextLink
+            href={href}
             className={twMerge(
                 'relative flex w-full cursor-pointer items-center gap-3 whitespace-nowrap px-4 text-zinc-500 transition-colors hover:bg-zinc-100',
                 active
                     ? 'font-semibold text-zinc-700 before:absolute before:bottom-[-1px] before:left-0 before:block before:h-[1px] before:w-full before:bg-zinc-700'
                     : ' hover:text-zinc-800'
             )}
-            onClick={() => void router.push(href)}
         >
             <Icon className='h-6 w-6 text-inherit' weight='duotone' />
             <span className='hidden sm:block'>{children}</span>
-        </a>
+        </NextLink>
     );
 };
 
